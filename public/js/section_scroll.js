db0a5d1e00096d548c17b6873eafbf2d8ea88655
Luka Samac <samacl@mcmaster.ca>
2021-12-12 19:50:25 -0500

    fading landing on first scroll

--- public/js/section_scroll.js
+++ public/js/section_scroll.js
@@ -1,38 +1,41 @@
 $(document).ready(function(){
-    console.log('running!!!');
-
     const scrollElement =
         window.document.scrollingElement ||
         window.document.body ||
         window.document.document;
 
     var scroll = {
-    activeSection: 0,
-    totalSections: document.getElementsByTagName('section').length,
-    throttled: false,
-    throttleDur: 500,
+        activeSection: 0,
+        totalSections: document.getElementsByTagName('section').length,
+        throttled: false,
+        throttleDur: 500,
     }
 
     var downSection = () => {
         if (scroll.activeSection < scroll.totalSections) {
-            ++scroll.activeSection
-            scrollToSection(scroll.activeSection)
+            ++scroll.activeSection;
+            scrollToSection(scroll.activeSection);
         }
     }
 
     var upSection = () => {
         if (scroll.activeSection > 0) {
-            --scroll.activeSection
-            scrollToSection(scroll.activeSection)
+            --scroll.activeSection;
+            scrollToSection(scroll.activeSection);
         }
     }
 
     var scrollToSection = (section) => {
+        $('.fade-in').removeClass('is-visible');
+
         anime({
             targets: scrollElement,
             scrollTop: (section) * window.innerHeight,
             duration: scroll.throttleDur,
-            easing: 'linear'
+            easing: 'linear',
+            complete: () => {
+                $(`section:nth-child(${section}) .fade-in`).addClass('is-visible');
+            }
         })
 
         scroll.activeSection = section
@@ -40,7 +43,7 @@ $(document).ready(function(){
 
     window.addEventListener("keydown", function(e) {
         if ([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
-            e.preventDefault()
+            e.preventDefault();
         }
 
         if(!scroll.throttled) {
@@ -103,24 +106,30 @@ $(document).ready(function(){
             scroll.throttled = true
             
             setTimeout(function() {
-            scroll.throttled = false
-            }, 1.5 * scroll.throttleDur)
+                scroll.throttled = false;
+            }, 1.5 * scroll.throttleDur);
             
             if (diffY > 0) {
-            downSection()
-            } else {
-            upSection()
+                downSection();
+            } 
+            else {
+                upSection();
             }
         }
 
-        initialy = null
+        initialy = null;
 
-    }, {passive: false})
+    }, {passive: false});
 
 
     // Scroll back to correct section when resized.
     window.addEventListener('resize', function(e) {
-    scrollToSection(scroll.activeSection)
-    }, false)
+        scrollToSection(scroll.activeSection);
+    }, false);   
 
+    $(window).on('scroll', function(e){
+        var pos = $(window).scrollTop();
+        var vh = $(window).height();
+        $('.landing-overlay').css('opacity', (pos / vh));
+    });
 });
