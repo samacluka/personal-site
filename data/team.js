f04dfe6171746f94f9d3ba01d78273e95658c437
Luka Samac <samacl@mcmaster.ca>
2021-12-19 17:56:06 -0500

    moving team to a file instead of hard coding in the front end

---
+++ data/team.js
@@ -0,0 +1,12 @@
+module.exports = [
+    {
+        name: "Luka Samac",
+        image: "/images/luka_samac.png",
+        description: "Graduating with a degree in Mechatronics Engineering and Management from McMaster University Luka consistently trades knowledge depth for breadth. Exemplified by his past experiences from working on drone research and the automation industry to managing funds over $200,000 Luka draws on an array of experiences to develop unique solutions. Personally, Luka enjoys being outside and active. If you can't find him portaging he may be rock climbing instead."
+    },
+    {
+        name: "Justin Ballaro",
+        image: "",
+        description: "Coolest guy alive."
+    }
+];
--- routes/callbacks.js
+++ routes/callbacks.js
@@ -9,11 +9,24 @@ var callbacks = {
       },
   }
 };
+// ======================================= HELPER ========================================
+Array.prototype.chunk = function(groupsize){
+  var sets = [], chunks, i = 0;
+  chunks = Math.ceil(this.length / groupsize);
+
+  while(i < chunks){
+      sets[i++] = this.splice(0, groupsize);
+  }
+
+  return sets;
+};
+
 
 // ======================================== INDEX ========================================
 // GET
 callbacks.index.get.landing = function(req,res){
-  return res.render(views.index.landing);
+  var team = require(rootDir+'data/team.js').chunk(3);
+  return res.render(views.index.landing, {team: team});
 };
 
 // ======================================== EXPORT ========================================
