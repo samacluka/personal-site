d4ad1c111039e4b1be9b9c8d73e78f273598790d
Luka Samac <samacl@mcmaster.ca>
2021-12-29 09:53:08 -0500

    cleaning up member cards

--- data/team.js
+++ data/team.js
@@ -2,7 +2,21 @@ module.exports = [
     {
         name: "Luka Samac",
         image: "/images/luka_samac.png",
-        description: "Graduating with a degree in Mechatronics Engineering and Management from McMaster University Luka consistently trades knowledge depth for breadth. Exemplified by his past experiences from working on drone research and the automation industry to managing funds over $200,000 Luka draws on an array of experiences to develop unique solutions. Personally, Luka enjoys being outside and active. If you can't find him portaging he may be rock climbing instead."
+        description: "Graduating with a degree in Mechatronics Engineering and Management from McMaster University Luka consistently trades knowledge depth for breadth. Exemplified by his past experiences from working on drone research and the automation industry to managing funds over $200,000 Luka draws on an array of experiences to develop unique solutions. Personally, Luka enjoys being outside and active. If you can't find him portaging he may be rock climbing instead.",
+        socials: [
+            {
+                link: "https://ca.linkedin.com/in/luka-samac",
+                icon: "fab fa-linkedin"
+            },
+            {
+                link: "mailto:1lukasamac@gmail.com",
+                icon: "fas fa-envelope"
+            },
+            {
+                link: "tel:9055207910",
+                icon: "fas fa-phone"
+            },
+        ]
     },
     {
         name: "Justin Ballaro",
