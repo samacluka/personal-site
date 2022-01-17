7a2cec6df2f0b554c478f767be01513d47c4ab1f
Luka Samac <samacl@mcmaster.ca>
2022-01-17 18:26:19 -0500

    changing order of json fields

--- data/team.js
+++ data/team.js
@@ -2,9 +2,9 @@ module.exports = [
     {
         name: "Luka Samac",
         image: "/images/luka_samac.png",
+        description: "Graduating with a degree in Mechatronics Engineering and Management from McMaster University Luka consistently trades knowledge depth for breadth. Exemplified by his past experiences from working on drone research and the automation industry to managing funds over $200,000 Luka draws on an array of experiences to develop unique solutions. Personally, Luka enjoys being outside and active. If you can't find him portaging he may be rock climbing instead.",
         credentials: "EIT",
         position: "Partner",
-        description: "Graduating with a degree in Mechatronics Engineering and Management from McMaster University Luka consistently trades knowledge depth for breadth. Exemplified by his past experiences from working on drone research and the automation industry to managing funds over $200,000 Luka draws on an array of experiences to develop unique solutions. Personally, Luka enjoys being outside and active. If you can't find him portaging he may be rock climbing instead.",
         socials: [
             {
                 link: "https://ca.linkedin.com/in/luka-samac",
