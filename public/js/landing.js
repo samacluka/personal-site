922f534976665cc4568a1ebc74a42f8094cc997f
Luka Samac <samacl@mcmaster.ca>
2022-01-22 10:39:58 -0500

    making the about section more responsive

--- public/js/landing.js
+++ public/js/landing.js
@@ -1,26 +1,32 @@
+function fadeSection(){
+    var pos = $(window).scrollTop();
+
+    $('#landing, #about').each(($i, $s, $ss) => {
+        $s = $($s);
+        var $overlay = $s.find('.overlay');
+        var offset = $s.offset().top;
+        var height = $s.height();
+        var posDelay = 0.6; // start effect when this % of the section is scrolled past
+        var startPos = (offset + posDelay * height); // position when the effect starts (section specific)
+        // var endPos = offset + height; // position when the opacity should be >= 1
+
+        if(pos > startPos){
+            var opacity = (pos - startPos) / (height * (1 - posDelay));
+            if(opacity < 0) opacity = 0;
+            else if(opacity > 1) opacity = 1;
+
+            $overlay.show().css('opacity', opacity);
+        }
+        else {
+            $overlay.hide();
+        }
+    });
+}
+
 $(document).ready(function(){
     /* Fade Effect */
-    $(window).on('scroll', function(e){
-        var vh = $(window).height();
-        var pos = $(window).scrollTop();
-
-        $('#landing, #about').each(($i, $s, $ss) => {
-            $s = $($s);
-            var $overlay = $s.find('.overlay');
-            var offset = $s.offset().top;
-
-            if(pos > offset){
-                var opacity = (pos - offset) / vh;
-                if(opacity < 0) opacity = 0;
-                else if(opacity > 1) opacity = 1;
-
-                $overlay.show().css('opacity', opacity);
-            }
-            else {
-                $overlay.hide();
-            }
-        });
-    });
+    $(window).on('scroll', fadeSection);
+    fadeSection(); // on page load
 
     $('.card').on('click', function(e){
         $card = $(this);
